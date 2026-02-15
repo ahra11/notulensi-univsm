e66c7ee6cbd7417fcfb01eba43cc2129751641ee
ahra11 <38848989+ahra11@users.noreply.github.com>
2026-02-16 00:18:21 +0800

    Update spreadsheet.ts

--- services/spreadsheet.ts
+++ services/spreadsheet.ts
@@ -2,7 +2,7 @@
  * Spreadsheet Service - Universitas Sapta Mandiri
  * Optimasi: Menambahkan Cache-Buster agar data Online selalu segar
  */
-const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbz_R0sOiuJtztp2lz0DHkG40S755Gh389RZienuYXzpaPeNrbUmQMd5yErzBEsGx1mnnQ/exec";
+const WEB_APP_URL = "https://script.google.com/macros/s/AKfycbxZTAAZKzaVH2kofzYfmg1W8aizdrqbIhrM_tAE1_Y4Ip6uOHpFWISEjX1FGv5gGEkVJQ/exec";
 
 export const SpreadsheetService = {
     // ==========================================
